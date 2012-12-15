52113f3c703c53e5d49dfbd59c2a896185199926
Mattias Andrée <maandree@operamail.com>
2012-12-15 05:44:27 +0100

    files + cpio list generator

--- Makefile
+++ Makefile
@@ -1,17 +1,21 @@
-all: makedirs makelinks
+all: dirs linkdirs linkfiles touches
 
-makedirs:
+dirs:
 	mkdir -p fs/"dev"
 	mkdir -p fs/"proc"
 	mkdir -p fs/"run"
 	mkdir -p fs/"sys"
 	mkdir -p fs/"tmp"
-	mkdir -p fs/"etc"
+	mkdir -p fs/"etc/udev"
+	mkdir -p fs/"new_root"
 	mkdir -p fs/"usr/bin"
 	mkdir -p fs/"usr/lib"
 	mkdir -p fs/"usr/local"
 
-makelinks:
+	chmod 700 fs/"new_root"
+
+
+linkdirs:
 	if [ -e fs/"bin" ]; then  $(RM) fs/"bin";  fi
 	if [ -e fs/"lib" ]; then  $(RM) fs/"lib";  fi
 	if [ -e fs/"sbin" ]; then  $(RM) fs/"sbin";  fi
@@ -19,11 +23,24 @@ makelinks:
 	if [ -e fs/"usr/local/bin" ]; then  $(RM) fs/"usr/local/bin";  fi
 	if [ -e fs/"usr/local/lib" ]; then  $(RM) fs/"usr/local/lib";  fi
 	if [ -e fs/"usr/local/sbin" ]; then  $(RM) fs/"usr/local/sbin";  fi
+
 	ln -s "usr/bin" fs/"bin"
 	ln -s "usr/lib" fs/"lib"
 	ln -s "usr/bin" fs/"sbin"
-	ln -s "usr/bin" fs/"usr/sbin"
-	ln -s "usr/bin" fs/"usr/local/bin"
-	ln -s "usr/lib" fs/"usr/local/lib"
-	ln -s "usr/bin" fs/"usr/local/sbin"
+	ln -s "bin" fs/"usr/sbin"
+	ln -s "../bin" fs/"usr/local/bin"
+	ln -s "../lib" fs/"usr/local/lib"
+	ln -s "../bin" fs/"usr/local/sbin"
+
+
+linkfiles:
+	if [ -e fs/"etc/mtab" ]; then  $(RM) fs/"etc/mtab";  fi
+
+	ln -s "/proc/self/mounts" fs/"mtab"
+
+
+touches:
+	touch fs/"etc/udev/udev.conf"
+	touch fs/"etc/fstab"
+	touch fs/"etc/initrd-release"
 
---
+++ cpiolist.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+'''
+
+USAGE:  find (root_directory) | ./cpiolist.py (root_directory) > (cpio_list)
+
+'''
+import sys
+import os
+
+
+def main(root):
+    while True:
+        try:
+            location = input()
+            name = location[len(root):]
+            if (name == '/') or (name == ''):
+                continue
+            
+            stat = os.stat(location, follow_symlinks=False)
+            uid = stat.st_uid
+            gid = stat.st_gid
+            mode = stat.st_mode
+            mod = oct(mode)[-4:]
+            
+            if os.path.isfile(location):
+                print("file %s %s %s %i %i" % (name, location, mod, uid, gid))
+            elif os.path.isdir(location):
+                print("dir %s %s %i %i" % (name, mod, uid, gid))
+            elif os.path.islink(location):
+                print("slink %s %s %s %i %i" % (name, os.readlink(location), mod, uid, gid))
+            else:
+                t = oct(mode)[2:-4]
+                if t == '1':
+                    print("pipe %s %s %i %i" % (name, mod, uid, gid))
+                elif t == '14':
+                    print("sock %s %s %i %i" % (name, mod, uid, gid))
+                else:
+                    major = os.major(stat.st_rdev)
+                    minor = os.minor(stat.st_rdev)
+                    if t == '2':
+                        print("nod %s %s %i %i c %i %i" % (name, mod, uid, gid, major, minor))
+                    elif t == '6':
+                        print("nod %s %s %i %i b %i %i" % (name, mod, uid, gid, major, minor))
+        except EOFError:
+            break
+
+
+if __name__ == '__main__':
+    root = sys.argv[1]
+    main(root[:-1] if root.endswith('/') else root)
+
